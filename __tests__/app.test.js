3ddac0bcc9a1c77ee6722ab3c913e6a7f037dc32
jabc8 <jt.024@outlook.com>
2025-03-10 10:41:47 +0000

    set up first connection - retrieve a list of all endpoints

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,20 @@
 const endpointsJson = require("../endpoints.json");
-/* Set up your test imports here */
+const db = require("../db/connection");
+const data = require("../db/data/test-data");
+const seed = require("../db/seeds/seed");
+const request = require("supertest");
+const app = require("../app");
 
-/* Set up your beforeEach & afterAll functions here */
+beforeEach(() => seed(data));
+afterAll(() => db.end());
 
-xdescribe("GET /api", () => {
-  test("200: Responds with an object detailing the documentation for each endpoint", () => {
-    return request(app)
-      .get("/api")
-      .expect(200)
-      .then(({ body: { endpoints } }) => {
-        expect(endpoints).toEqual(endpointsJson);
-      });
-  });
+describe("GET /api", () => {
+    test("200: Responds with an object detailing the documentation for each endpoint", () => {
+        return request(app)
+            .get("/api")
+            .expect(200)
+            .then(({ body: { endpoints } }) => {
+                expect(endpoints).toEqual(endpointsJson);
+            });
+    });
 });
