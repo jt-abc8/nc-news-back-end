6e988bc067066a5b4c87218fc9364b40562b598d
jabc8 <jt.024@outlook.com>
2025-03-10 17:09:23 +0000

    add new endpoint GET /api/articles/:article_id and app.use methods for new error handling functions

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const { getEndpoints } = require("./controllers/api.controller");
-const { getTopics } = require("./controllers/topics.controller");
+const {
+    getTopics,
+    getArticleByID,
+} = require("./controllers/topics.controller");
+const {
+    handleCustomErrors,
+    handlePsqlErrors,
+} = require("./controllers/errors.controller");
 
 const app = express();
 
 app.get("/api", getEndpoints);
 app.get("/api/topics", getTopics);
+app.get("/api/articles/:article_id", getArticleByID);
 app.all("/api/*", (req, res) => {
-    res.status(404).send({ msg: "404 Not Found"});
-})
+    res.status(404).send({ msg: "404 Not Found" });
+});
+
+app.use(handleCustomErrors);
+app.use(handlePsqlErrors);
 
 module.exports = app;
