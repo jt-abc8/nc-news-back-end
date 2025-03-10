44ce20e299f4e2e11ccde54baffc74c220c5ebf5
jabc8 <jt.024@outlook.com>
2025-03-10 17:17:35 +0000

    test for new endpoint - testing 200 ok, 404 not found, and 400 bad request

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,6 +4,7 @@ const data = require("../db/data/test-data");
 const seed = require("../db/seeds/seed");
 const request = require("supertest");
 const app = require("../app");
+const { convertTimestampToDate } = require("../db/seeds/utils");
 
 beforeEach(() => seed(data));
 afterAll(() => db.end());
@@ -34,14 +35,60 @@ describe("GET /api/topics", () => {
                 });
         });
     });
+    describe("404 Not Found", () => {
+        test("Responds with a 404 Not Found message when endpoint is not found", () => {
+            return request(app)
+                .get("/api/not-topics")
+                .expect(404)
+                .then(({ body: { msg } }) => {
+                    expect(msg).toBe("404 Not Found");
+                });
+        });
+    });
 });
-describe("404 Not Found", () => {
-    test("Responds with a 404 Not Found message when endpoint is not found", () => {
-        return request(app)
-            .get("/api/not-topics")
-            .expect(404)
-            .then(({ body: { msg } }) => {
-                expect(msg).toBe("404 Not Found");
-            });
+
+describe("GET /api/articles/:article_id", () => {
+    describe("200 OK", () => {
+        test("Responds with data for a specific article", () => {
+            return request(app)
+                .get("/api/articles/4")
+                .expect(200)
+                .then(({ body: { article } }) => {
+                    const expected = convertTimestampToDate(
+                        data.articleData[3]
+                    );
+
+                    expect(article.article_id).toBe(4);
+                    expect(article.title).toBe(expected.title);
+                    expect(article.topic).toBe(expected.topic);
+                    expect(article.author).toBe(expected.author);
+                    expect(article.body).toBe(expected.body);
+                    expect(typeof article.created_at).toBe("string"); //!
+                    expect(article.votes).toBe(expected.votes);
+                    expect(article.article_img_url).toBe(expected.article_img_url);
+                });
+        });
+    });
+    describe("404 Not Found", () => {
+        test("Responds with a 404 Not Found message when article is not found", () => {
+            return request(app)
+                .get("/api/articles/412")
+                .expect(404)
+                .then(({ body: { msg } }) => {
+                    expect(msg).toBe("404 Not Found");
+                });
+        });
+    });
+    describe("400 Bad Request", () => {
+        test(
+            "Responds with a 400 Bad Request message when article_id parameter is invalid", () => {
+              return request(app)
+              .get("/api/articles/not-a-valid-article")
+              .expect(400)
+              .then(({ body: { msg } }) => {
+                  expect(msg).toBe("400 Bad Request");
+              });
+            }
+        );
     });
 });
