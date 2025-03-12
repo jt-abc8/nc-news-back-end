73f91e983fcf1b0fd06f0cee6da551da10a1529a
jabc8 <jt.024@outlook.com>
2025-03-12 13:12:23 +0000

    refactor comments.model.js to use checkExists utils function

--- models/comments.model.js
+++ models/comments.model.js
@@ -1,13 +1,17 @@
 const db = require("../db/connection");
-const { selectArticleByID } = require("./articles.model");
+const { checkExists } = require("../db/seeds/utils");
 
 exports.selectCommentsByArticleID = (article_id) => {
-    return selectArticleByID(article_id)
-        .then(() => {
-            return db.query(
-                "SELECT * FROM comments WHERE article_id = $1 ORDER BY created_at DESC",
-                [article_id]
-            );
+    return checkExists("articles", "article_id", article_id)
+        .then((exists) => {
+            if (exists) {
+                return db.query("SELECT * FROM comments WHERE article_id = $1 ORDER BY created_at DESC", [article_id]);
+            } else {
+                return Promise.reject({
+                    status: 404,
+                    msg: "404 Not Found",
+                });
+            }
         })
         .then(({ rows }) => rows);
 };
@@ -16,13 +20,21 @@ exports.insertComment = (article_id, username, body) => {
     if (!username || !body) {
         return Promise.reject({ status: 400, msg: "400 Bad Request" });
     } else {
-        return selectArticleByID(article_id).then(() => {
-            return db
-                .query(
-                    "INSERT INTO comments(article_id, author, body) VALUES($1, $2, $3) RETURNING *",
-                    [article_id, username, body]
-                )
-                .then(({ rows }) => rows[0]);
+        return checkExists("articles", "article_id", article_id).then((exists) => {
+            if (exists) {
+                return db
+                    .query("INSERT INTO comments(article_id, author, body) VALUES($1, $2, $3) RETURNING *", [
+                        article_id,
+                        username,
+                        body,
+                    ])
+                    .then(({ rows }) => rows[0]);
+            } else {
+                return Promise.reject({
+                    status: 404,
+                    msg: "404 Not Found",
+                });
+            }
         });
     }
 };
