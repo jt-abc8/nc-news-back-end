ebb0dc2fe963864daaaae8c66f8b0344621c7e9f
jabc8 <jt.024@outlook.com>
2025-03-14 09:25:37 +0000

    add code to facilitate hosting

--- db/connection.js
+++ db/connection.js
@@ -5,8 +5,14 @@ const ENV = process.env.NODE_ENV || "development";
 const path = `${__dirname}/../.env.${ENV}`;
 require("dotenv").config({ path });
 
-if (!process.env.PGDATABASE) {
-    throw new Error("No PGDATABASE configured");
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+    throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
-module.exports = new Pool();
+const config = {};
+if (ENV === "production") {
+    config.connectionString = process.env.DATABASE_URL;
+    config.max = 2;
+}
+
+module.exports = new Pool(config);
