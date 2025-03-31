b6c5ebed559e4bdde0962aee3dc849e021f6361f
jabc8 <jt.024@outlook.com>
2025-03-31 10:11:04 +0100

    allow cross-origin resource sharing

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const apiRouter = require("./routers/api-router")
-
+const cors = require("cors");
 const { handleCustomErrors, handlePsqlErrors, handleServerErrors } = require("./controllers/errors.controller");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use("/api", apiRouter);
 app.use(handleCustomErrors);
